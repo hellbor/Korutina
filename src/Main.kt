012294b4e0941ac126e717c4b5a90a3d0661e0a4
Ольга <hellbor@mail.ru>
2026-02-03 20:13:32 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,31 @@
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    //1
+    val job = launch {
+        repeat(5) {
+            println("Работаю в корутине")
+            delay(100L)
+        }
+    }
+    repeat(5) {
+        println("Работаю в main")
+        delay(100L)
+    }
+    job.join()
+
+    //2
+    val deferredResult = async {
+        println("Начало вычислений")
+        delay(1000L)
+        return@async 10
+    }
+    println("Корутина запущена")
+
+    val result = deferredResult.await()
+    println("Результат: $result")
+}
